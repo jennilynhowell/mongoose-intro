2d6fdc21b477792aec98b71fd15b0b494310d9a6
Jennilyn Howell <jennilynhowell@me.com>
2017-07-06 09:47:01 -0400

    mongoose intro, model setup step 1

---
+++ models/recipe.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+  name: {type: String, required: true, unique: true},
+  prepTime: Number,
+  cookTime: Number,
+  ingredients: [{ //NOTE THE STRUCTURE OF THIS
+    amount: {type: Number, required: true, default: 1},
+    name: {type: String, required: true}
+  }],
+  instructions: [String]
+});
