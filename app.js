96b1ec9a38ac3d916f88140aea23a9c98e33c5e9
Jennilyn Howell <jennilynhowell@me.com>
2017-07-06 09:59:49 -0400

    rm node_modules

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose');
+const Recipe = require('./models/recipe');
+
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost/27017/mongoosestore');
+
+const newRecipe = new Recipe({name: 'Salad'});
+
+console.log(newRecipe.toObject());
