e79d49234bcf1c20d03fe34034489ab151070d7a
Jennilyn Howell <jennilynhowell@me.com>
2017-07-06 11:56:45 -0400

    find and update... in progress

--- app.js
+++ app.js
@@ -4,6 +4,35 @@ const Recipe = require('./models/recipe');
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost/27017/mongoosestore');
 
-const newRecipe = new Recipe({name: 'Salad'});
+// const newRecipe = new Recipe({name: 'Salad'});
+// const ingredient = {amount: 5, name: 'cherry tomatoes'};
+// newRecipe.ingredients.push(ingredient);
+// newRecipe.save(); //sends it to the database
+// console.log(newRecipe.toObject());
 
-console.log(newRecipe.toObject());
+// //use this in your app.get
+// Recipe.find({}).then(results => {
+//   console.log(results);
+//   //res.render...
+// });
+//
+// //remember this will return an object and you can use dot notation on it
+// Recipe.findOne({name: 'Salad'}).then(result => {
+//   console.log(result);
+// });
+
+//updateone: the verbose way
+Recipe.findOne({name: 'Salad'}).then(result => {
+  const ingredient = {name: 'cucumbers', amount: 1};
+  result.ingredients.push(ingredient);
+  result.save();
+});
+
+//updateone: betterway
+Recipe.updateOne({name: 'Salad'},
+  {
+    $push: {
+      ingredients: {name: 'olives', amount: 3}
+    }
+  }
+);
